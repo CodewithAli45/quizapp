d0c5d6448bd0a522939d3e79fdd1d51c23c8aff8
CodewithAli45 <ali.my0304@gmail.com>
2024-09-12 01:40:06 +0530

    previos verioson

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation  } from 'react-router-dom'
+import { BrowserRouter, Routes, Route  } from 'react-router-dom'
 import './App.css';
 import QuizController from './components/QuizController';
 import Questions from './components/Questions';
-import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
 export default function App() {
-    const location = useLocation();
   return (
     <BrowserRouter >
         <div className='main-bg'>
-        <SwitchTransition mode="out-in">
-          <CSSTransition
-            key={location.key}
-            classNames="fade"
-            timeout={300} // Duration of the transition
-          >
             <Routes location={location}>
               <Route path="/" element={<QuizController />} />
               <Route path="/question" element={<Questions />} />
             </Routes>
-          </CSSTransition>
-        </SwitchTransition>
         </div>
     </BrowserRouter>
   )
